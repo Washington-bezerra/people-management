bbdf0bc38dce7a5fef7b98be6a5374ded71ce160
Washington Bezerra <61431230+Washington-bezerra@users.noreply.github.com>
2023-01-11 23:33:19 -0300

    Alter table: set birth_date as date

--- src/main/resources/db.migration/V001_create_people_table.sql
+++ src/main/resources/db.migration/V001_create_people_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE people (
     id UUID PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
-    birth_date TIMESTAMP NOT NULL
+    birth_date DATE NOT NULL
 );
 
 CREATE TABLE address (
