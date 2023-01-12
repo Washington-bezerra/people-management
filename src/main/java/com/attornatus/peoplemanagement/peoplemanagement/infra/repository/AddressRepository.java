c3f4889b8d352c9162ff9852cc758d2f4f906c11
Washington Bezerra <61431230+Washington-bezerra@users.noreply.github.com>
2023-01-12 19:53:14 -0300

    created findByPeopleId in repository

--- src/main/java/com/attornatus/peoplemanagement/peoplemanagement/infra/repository/AddressRepository.java
+++ src/main/java/com/attornatus/peoplemanagement/peoplemanagement/infra/repository/AddressRepository.java
@@ -4,7 +4,10 @@ import com.attornatus.peoplemanagement.peoplemanagement.infra.entity.AddressEnti
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 @Repository
 public interface AddressRepository extends JpaRepository<AddressEntity, UUID> {
+    List<AddressEntity> findByPeopleId(UUID peopleId);
+
 }
